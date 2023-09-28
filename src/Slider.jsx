b4892dc69098e52a9614ea8eeed76cee99eb8954
Manan Chataut <mananchataut@gmail.com>
2023-09-28 20:27:09 +0530

    Add: Initial Files

---
+++ src/Slider.jsx
@@ -0,0 +1,16 @@
+function Slider({ min, max, value, handleChange }) {
+  return (
+    <div className="slider-container">
+      <input
+        type="range"
+        className="slider"
+        min={min}
+        max={max}
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default Slider;
